8fc05657366ea780f3dbed417bfa62aaa5163541
tripham <tri.pham@myy.haaga-helia.fi>
2025-12-14 18:06:08 +0200

    Initial commit

---
+++ mongo_connection.py
@@ -0,0 +1,35 @@
+from bson import ObjectId
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["library"]
+
+print("Collections:", db.list_collection_names())
+
+print("Books in database:\n")
+
+for book in db.books.find().limit(5):
+    print(book.get("title"), "-", book.get("author"))
+
+book_id_to_find = "6780b2d277f48b749b940ee4"
+
+db.books.find_one({ "_id": ObjectId(book_id_to_find) })
+
+
+def add_book():
+    author = input("Author:")
+    title = input("Title: ")
+    year = int(input("Year: "))
+
+    book = {
+        "title": title,
+        "author": author,
+        "year": year
+    }
+
+    result = db.books.insert_one(book)
+    return result.inserted_id
+
+_id = add_book()
+print("\nInserted document ID:", _id)
